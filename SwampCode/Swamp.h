f1182d3ccf26fb45581c8aff37f1156e835a8756
robert11arad <robert11arad@gmail.com>
2017-03-17 18:04:33 +0000

    Add files via upload

---
+++ SwampCode/Swamp.h
@@ -0,0 +1,24 @@
+#ifndef SWAMP_H
+#define SWAMP_H
+
+#include <iostream>
+
+#include <cstdio>
+
+using namespace std;
+
+
+class Swamp
+{
+    public:
+        Swamp();
+        virtual ~Swamp();
+
+        void Enter();
+
+
+
+    private:
+};
+
+#endif // SWAMP_H
